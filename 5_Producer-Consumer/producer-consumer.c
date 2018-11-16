0cba43ca4ff132fcd013040e575f31d8e4cb30cb
Bharath Kumar <kumarbharath718@gmail.com>
2018-11-17 00:12:03 +0530

    use threads
    modify producer consumer problem to use threads

--- 5_Producer-Consumer/producer-consumer.c
+++ 5_Producer-Consumer/producer-consumer.c
@@ -1,69 +1,108 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <semaphore.h>
 
-int mutex=1,full=0,empty=3,x=0;
+pthread_t *producers;
+pthread_t *consumers;
 
-int main()
-{
-	int n;
-	void producer();
-	void consumer();
-	int wait(int);
-	int signal(int);
-	printf("\n1.Producer\n2.Consumer\n3.Exit\n");
-	while(1)
-	{
-		printf("\nEnter your choice:");
-		scanf("%d",&n);
-		switch(n)
-		{
-			case 1:    if((mutex==1)&&(empty!=0))
-									 producer();
-								 else
-									 printf("Buffer is full!!\n");
-								 break;
-			case 2:    if((mutex==1)&&(full!=0))
-									 consumer();
-								 else
-									 printf("Buffer is empty!!\n");
-								 break;
-			case 3: 
-								 exit(0);
-								 break;
-			default:   printf("Wrong choice!!\n");
-								 break;
-		}
+sem_t buf_mutex, empty_count, fill_count;
+
+int *buf, buf_pos=-1, prod_count, con_count, buf_len;
+
+int produce(pthread_t self){
+	int i = 0;
+	int p = 1 + rand()%40;
+	while(!pthread_equal(*(producers+i),self) && i < prod_count){
+		i++;
+	}
+	printf("\nProducer %d produced %d \n", i+1, p);
+	return p;
+}
+
+
+void consume(int p,pthread_t self){
+	int i = 0;
+	while(!pthread_equal(*(consumers+i),self) && i < con_count){
+		i++;
+	}
+
+	printf("\n\tConsumer %d consumed %d \n", i+1, p);
+
+}
+
+
+void* producer(void *args){
+
+	while(1){
+		int p = produce(pthread_self());
+		sem_wait(&empty_count);
+		sem_wait(&buf_mutex);
+		++buf_pos;			// critical section
+		*(buf + buf_pos) = p; 
+		sem_post(&buf_mutex);
+		sem_post(&fill_count);
+		sleep(1 + rand()%3);
+	}
+	
+	return NULL;
+}
+
+
+void* consumer(void *args){
+	int c;
+	while(1){
+		sem_wait(&fill_count);
+		sem_wait(&buf_mutex);
+		c = *(buf+buf_pos);
+		consume(c,pthread_self());
+		--buf_pos;
+		sem_post(&buf_mutex);
+		sem_post(&empty_count);
+		sleep(1+rand()%5);
+	}
+
+	return NULL;
+}
+
+int main() {
+	
+	srand(time(NULL));
+
+	sem_init(&buf_mutex, 0, 1);
+	sem_init(&fill_count, 0, 0);
+
+	printf("Enter the number of Producers: ");
+	scanf("%d",&prod_count);
+	producers = (pthread_t*) malloc(prod_count*sizeof(pthread_t));
+
+	printf("Enter the number of Consumers: ");
+	scanf("%d", &con_count);
+	consumers = (pthread_t*) malloc(con_count*sizeof(pthread_t));
+
+	printf("Enter buffer capacity: ");
+	scanf("%d", &buf_len);
+	buf = (int*) malloc(buf_len*sizeof(int));
+
+	sem_init(&empty_count, 0, buf_len);
+
+	for(int i=0; i<prod_count; i++) {
+		pthread_create(producers+i, NULL, &producer, NULL);
+	}
+
+	for(int i=0; i<con_count; i++){
+		pthread_create(consumers+i, NULL, &consumer, NULL);
+	}
+
+	for(int i=0; i<prod_count; i++){
+		pthread_join(*(producers+i), NULL);
+	}
+	for(int i=0; i<con_count; i++){
+		pthread_join(*(consumers+i), NULL);
 	}
 
 	return 0;
-}
-
-int wait(int s)
-{
-	return (--s);
-}
-
-int signal(int s)
-{
-	return(++s);
-}
-
-void producer()
-{
-	mutex=wait(mutex);
-	full=signal(full);
-	empty=wait(empty);
-	x++;
-	printf("\nProducer produces the item %d\n",x);
-	mutex=signal(mutex);
-}
-
-void consumer()
-{
-	mutex=wait(mutex);
-	full=wait(full);
-	empty=signal(empty);
-	printf("\nConsumer consumes item %d\n",x);
-	x--;
-	mutex=signal(mutex);
 }
